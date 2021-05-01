9e0ebc41825227c1ab48e83fab3eb627af706c9b
Bunyawat <stonksfarm37204@gmail.com>
2021-05-01 19:48:27 +0700

    bot can get user input

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require('dotenv').config();
 
 const { Client } = require('discord.js');
 const botnaja = new Client();
@@ -9,4 +9,9 @@ botnaja.login(process.env.TOKEN);
 //to make sure your bot log in
 botnaja.on('ready',()=>{
     console.log('botnaja has logged in');
+});
+
+//get user input
+botnaja.on('message',(msg)=>{
+    console.log(`${msg.author.tag} : ${msg.content}`);
 });
