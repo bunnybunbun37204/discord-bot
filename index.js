6171eecc77f4341e3af81acb093526b8fdb4a59d
Bunyawat <stonksfarm37204@gmail.com>
2021-05-01 21:54:13 +0700

    add command

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const { Client } = require('discord.js');
+const { Client } = require("discord.js");
 const botnaja = new Client();
+const PREFIX_COMMAND = "!"; //to make all command begins with '!'
 
 // login bot token is in .env file
-botnaja.login(process.env.TOKEN); 
+botnaja.login(process.env.TOKEN);
 
 //to make sure your bot log in
-botnaja.on('ready',()=>{
-    console.log('botnaja has logged in');
+botnaja.on("ready", () => {
+  console.log("botnaja has logged in");
 });
 
 //get user input
-botnaja.on('message',(msg)=>{
-    console.log(`${msg.author.tag} : ${msg.content}`);
-    if(msg.content === 'test'){
-        msg.reply('อะหิ อะหิ');
-    }
+botnaja.on("message", (msg) => {
+  console.log(`${msg.author.tag} : ${msg.content}`);
+  if (msg.author.bot) return;
+  if (msg.content === "test4263") {
+    msg.reply("อะหิ อะหิ");
+  }
+  if (msg.content.startsWith(PREFIX_COMMAND)) {
+    const [real_command, ...args] = msg.content
+      .trim()
+      .substring(PREFIX_COMMAND.length)
+      .split(/\s+/);
+    botnaja_command(real_command, args);
+  }
 });
+
+const botnaja_command = (command, args) => {
+  console.log(command);
+  console.log(args);
+};
