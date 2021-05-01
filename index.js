7d628591afe135bec26073643751854d57fc55ad
Bunyawat <stonksfarm37204@gmail.com>
2021-05-01 19:42:53 +0700

    make bot joins to server

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ require("dotenv").config();
 const { Client } = require('discord.js');
 const botnaja = new Client();
 
-
 // login bot token is in .env file
 botnaja.login(process.env.TOKEN); 
+
+//to make sure your bot log in
+botnaja.on('ready',()=>{
+    console.log('botnaja has logged in');
+});
