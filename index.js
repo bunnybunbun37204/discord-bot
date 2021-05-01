7cacdfb46fd5a5d47818e51fa5ad994992a9e7f9
Bunyawat <stonksfarm37204@gmail.com>
2021-05-01 19:39:15 +0700

    add token

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 
-const { Client} = require('discord.js');
-const client = new Client();
+const { Client } = require('discord.js');
+const botnaja = new Client();
+
 
 // login bot token is in .env file
-client.login(process.env.DISCORD_BOT_TOKEN); 
+botnaja.login(process.env.TOKEN); 
