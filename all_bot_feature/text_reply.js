00b70bb66c3f08c432a77e54a51cfab28e19bc60
Bunyawat <stonksfarm37204@gmail.com>
2021-05-02 16:18:45 +0700

    delete aiml

--- all_bot_feature/text_reply.js
+++ all_bot_feature/text_reply.js
@@ -1,17 +1,9 @@
-const AIMLParser = require("aimlparser");
-const aimlParser = new AIMLParser({ name:'BotNAJA' })
-aimlParser.load(['aimlfile.xml']);
 module.exports = {
   name: "reply",
   bot_command: [], //This command in bot
   cooldown: 0,
   description: "auto reply",
   reply(msg, isActive) {
-    console.log('TEXT REPLY STARTED');
-    aimlParser.getResult(msg.content, (answer, input) => {
-        msg.reply(answer);
-        console.log(`wildcard array is ${wildCardArray}`);
-        console.log(`input is ${input}`);
-    })
+    console.log("TEXT REPLY STARTED");
   },
 };
