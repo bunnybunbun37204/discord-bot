d26747400cf380e01f60b1f8113a2abe2be24770
Bunyawat <stonksfarm37204@gmail.com>
2021-05-02 13:44:05 +0700

    add video player teat

--- all_bot_feature/music.js
+++ all_bot_feature/music.js
@@ -83,6 +83,7 @@ module.exports = {
 }
 
 const video_player = async (guild, song) => {
+    console.log('LOG TEST THIS FUNC');
     const song_queue = queue.get(guild.id);
 
     //If no song is left in the server queue.
