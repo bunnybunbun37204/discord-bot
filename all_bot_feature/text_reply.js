326fd90bb2cb7f74dc440c5de19b7769663145f3
Bunyawat <stonksfarm37204@gmail.com>
2021-05-02 15:45:52 +0700

    add aiml.xml

--- all_bot_feature/text_reply.js
+++ all_bot_feature/text_reply.js
@@ -1,10 +1,12 @@
 const AIMLParser = require("aimlparser");
+const aimlParser = new AIMLParser({ name:'BotNAJA' })
 module.exports = {
   name: "reply",
   bot_command: [], //This command in bot
   cooldown: 0,
   description: "auto reply",
   reply(msg, isActive) {
+      aimlParser.load(['./'])
       if(msg.content === 'hello'){
           msg.reply('hello');
       }
