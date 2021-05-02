7916eb8355c95c40c603aa9cbbd7ad144b85802f
Bunyawat <stonksfarm37204@gmail.com>
2021-05-02 15:50:28 +0700

    test aiml reply message

--- all_bot_feature/text_reply.js
+++ all_bot_feature/text_reply.js
@@ -1,14 +1,17 @@
 const AIMLParser = require("aimlparser");
 const aimlParser = new AIMLParser({ name:'BotNAJA' })
+aimlParser.load(['./aimlfile.xml']);
+
 module.exports = {
   name: "reply",
   bot_command: [], //This command in bot
   cooldown: 0,
   description: "auto reply",
   reply(msg, isActive) {
-      aimlParser.load(['./'])
-      if(msg.content === 'hello'){
-          msg.reply('hello');
-      }
+    aimlParser.getResult(msg.content, (answer, wildCardArray, input) => {
+        msg.reply(answer);
+        console.log(`wildcard array is ${wildCardArray}`);
+        console.log(`input is ${input}`);
+    })
   },
 };
