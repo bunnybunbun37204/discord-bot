a7c217e06108c39bd47ed80c88d3c68cc31a7c83
Bunyawat <stonksfarm37204@gmail.com>
2021-05-02 19:25:27 +0700

    add image repy

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ require("dotenv").config();
 const { Client } = require("discord.js");
 const botnaja = new Client();
 const music = require("./all_bot_feature/music.js");
-const { reply } = require("./all_bot_feature/text_reply.js");
 const bot_reply = require("./all_bot_feature/text_reply.js");
 const PREFIX_COMMAND = "$"; //to make all command begins with '!'
 
