bf381acb6540d5e44f244c95e68d8633e1152cec
Bunyawat <stonksfarm37204@gmail.com>
2021-05-05 20:32:50 +0700

    add new lib

--- all_bot_feature/text_reply.js
+++ all_bot_feature/text_reply.js
@@ -2,6 +2,7 @@ import  Classifier  from 'ml-classify-text';
 import { readFile } from 'fs/promises';
 
 const data = JSON.parse(await readFile(new URL('./assets/data.json',import.meta.url)));
+const classify = new Classifier();
 export default class ReplyMSg {
   constructor(msg){
     this.msg = msg;
