0efb357ff4bf58f0b706e9a6dc8aa95c44844d85
Bunyawat <stonksfarm37204@gmail.com>
2021-05-05 17:03:30 +0700

    bugs fixed

--- all_bot_feature/text_reply.js
+++ all_bot_feature/text_reply.js
@@ -4,8 +4,6 @@ import * as use from '@tensorflow-models/universal-sentence-encoder';
 
 import comments from './assets/train.json';
 import comment_testing from './assets/test.json';
-const tf = require('@tensorflow/tfjs');
-const tf_node = require('@tensorflow/tfjs-node');
 
 export default class ReplyMSg {
   constructor(msg){
