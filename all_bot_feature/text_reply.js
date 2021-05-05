17ae1e5e5578a7d0399f722fb6d9f72737a10d8a
Bunyawat <stonksfarm37204@gmail.com>
2021-05-05 18:15:40 +0700

    model training

--- all_bot_feature/text_reply.js
+++ all_bot_feature/text_reply.js
@@ -9,6 +9,71 @@ const test = JSON.parse(await readFile(new URL('./assets/test.json',import.meta.
 export default class ReplyMSg {
   constructor(msg){
     this.msg = msg;
-    console.log('import reply class');
+    run();
   }
 }
+
+const encodeData = data => {
+  const sentences = data.map(comment => comment.text.toLowerCase());
+  const trainingData = use.load()
+      .then(model => {
+          return model.embed(sentences)
+              .then(embeddings => {
+                  return embeddings;
+              });
+      })
+      .catch(err => console.error('Fit Error:', err));
+
+  return trainingData
+};
+
+const outputData = tf.tensor2d(comments.map(comment => [
+  comment.intent === 'buy' ? 1 : 0,
+  comment.intent === 'none' ? 1 : 0,
+]));
+
+const model = tf.sequential();
+
+// Add layers to the model
+model.add(tf.layers.dense({
+    inputShape: [512],
+    activation: 'sigmoid',
+    units: 2,
+}));
+
+model.add(tf.layers.dense({
+    inputShape: [2],
+    activation: 'sigmoid',
+    units: 2,
+}));
+
+model.add(tf.layers.dense({ 
+    inputShape: [2],
+    activation: 'sigmoid',
+    units: 2,
+}));
+
+// Compile the model
+model.compile({
+    loss: 'meanSquaredError',
+    optimizer: tf.train.adam(.06), // This is a standard compile config
+});
+
+function run() {
+  Promise.all([
+      encodeData(comments),
+      encodeData(comment_testing)
+  ])
+  .then(data => {
+      const {
+          0: training_data,
+          1: testing_data,
+      } = data;
+
+      model.fit(training_data, outputData, { epochs: 200 })
+          .then(history => {
+              model.predict(testing_data).print();
+          });
+  })
+  .catch(err => console.log('Prom Err:', err));
+};
