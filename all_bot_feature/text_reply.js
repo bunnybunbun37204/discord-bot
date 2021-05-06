f8b4ed58629cc2ed90194ba70fe975f3f0b34e35
Bunyawat <stonksfarm37204@gmail.com>
2021-05-06 14:04:51 +0700

    can add msg classification and make a classify function

--- all_bot_feature/text_reply.js
+++ all_bot_feature/text_reply.js
@@ -3,14 +3,22 @@ import { readFile } from 'fs/promises';
 
 const datas = JSON.parse(await readFile(new URL('./assets/data.json',import.meta.url)));
 const whichX = new WhichX();
-let data = datas['data'];
-whichX.addLabels("pokemon");
-whichX.addData("pokemon", "loyal and bright yellow with a lightning shaped tail");
-let pet = whichX.classify("Its yellow and shoots light");
-console.log("It's a " + pet + "!");
+const data = datas['type'];
+
+const data_classifier = data => {
+  for(let i = 0;i < data.length;i++){
+    whichX.addLabels(data[i]['name']);
+    whichX.addData(data[i]['name'],data[i]['description']);
+  }
+}
+data_classifier(data);
+
+let text = whichX.classify("Hello how are you");
+console.log("It's a " + text + " type");
 
 export default class ReplyMSg {
   constructor(msg) {
     this.msg = msg;
   }
 }
+
