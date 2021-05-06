8b0f3a554ee6fe90f0e86858bd1c826f4e21ee3c
Bunyawat <stonksfarm37204@gmail.com>
2021-05-06 13:42:36 +0700

    add whichX lib it's very easy to use

--- all_bot_feature/text_reply.js
+++ all_bot_feature/text_reply.js
@@ -1,5 +1,13 @@
-// import { readFile } from 'fs/promises';
-// const data = JSON.parse(await readFile(new URL('./assets/data.json',import.meta.url)));
+import WhichX from 'whichx';
+import { readFile } from 'fs/promises';
+
+const datas = JSON.parse(await readFile(new URL('./assets/data.json',import.meta.url)));
+const whichX = new WhichX();
+let data = datas['data'];
+whichX.addLabels("pokemon");
+whichX.addData("pokemon", "loyal and bright yellow with a lightning shaped tail");
+let pet = whichX.classify("Its yellow and shoots light");
+console.log("It's a " + pet + "!");
 
 export default class ReplyMSg {
   constructor(msg) {
