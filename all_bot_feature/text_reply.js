d5c2975315a8cdace134a21fd2d020a1b6b484dd
Bunyawat <stonksfarm37204@gmail.com>
2021-05-06 10:09:11 +0700

    train file

--- all_bot_feature/text_reply.js
+++ all_bot_feature/text_reply.js
@@ -1,11 +1,20 @@
 import Fasttext from 'fasttext.js'; 
 import { readFile } from 'fs/promises';
 const data = JSON.parse(await readFile(new URL('./assets/data.json',import.meta.url)));
-const fasttext = new Fasttext();
+const fastText = new Fasttext({
+  serializeTo: './models',
+  trainFile: './assets/dataset.txt'
+});
+fastText.train()
+.then(done=> {
+    console.log("train done.");
+})
+.catch(error => {
+    console.error(error);
+});
 
 export default class ReplyMSg {
   constructor(msg){
     this.msg = msg;
-    console.log(data);
   }
 }
