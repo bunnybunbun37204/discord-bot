ebcdf9961982cd6811c6bf8559699bef15516d4e
Bunyawat <stonksfarm37204@gmail.com>
2021-05-06 21:19:24 +0700

    bugs fixed

--- all_bot_feature/text_reply.js
+++ all_bot_feature/text_reply.js
@@ -23,7 +23,6 @@ const find_id_injson = (type) => {
       }
     }
   }
-  console.log('memory is ' + memo);
 }
 
 const answer = id => {
@@ -43,6 +42,7 @@ export default class ReplyMSg {
       let ans_list = answer(find_id_injson(type));
       const index = Math.floor(Math.random() * ans_list.length);
       msg.reply(ans_list[index]);
+      console.log(memo);
     }
     else {
       console.log('the bot is inactive status');
