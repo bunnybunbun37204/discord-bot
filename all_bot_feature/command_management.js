379f1bf3af29bf448b4b8fbab66df4b38269f20b
Bunyawat <stonksfarm37204@gmail.com>
2021-05-07 13:20:22 +0700

    recoding

---
+++ all_bot_feature/command_management.js
@@ -0,0 +1,40 @@
+import Play from './music.js'
+
+const music = new Play();
+let isActive = false;
+
+export default class Command {
+    command(msg) {
+        const [real_command, ...args] = msg.content
+            .trim()
+            .substring(PREFIX_COMMAND.length)
+            .split(/\s+/);
+        if (real_command === "help") {
+            msg.reply(
+                `คำสั่ง ทั้งหมดมีดังนี้
+        \n1. ***$help*** เพื่อดูคำสั่งทั้งหมด
+        \n2. ***$play <music link or music name>*** เพื่อเล่นเพลงที่ต้องการ และเพิ่มเพลงในคิว 
+        \n3. ***$stop*** เพือหยุดเพลง
+        \n4. ***$skip*** เพื่อข้ามไปเล่นเพลงถัดไป
+        \n5. ***สนใจพัฒนา BotNAJA*** สามารถกดลิ้งค https://github.com/stonksfarm-development/discord-bot`
+            );
+        }
+        else if (real_command === 'play' || real_command === 'skip' || real_command === 'stop') {
+            music.execute(msg, args, real_command, botnaja);
+        }
+        else if (real_command === 'activeBotReply') {
+            isActive = true;
+            msg.channel.send(`ฟังก์ชันตอบโต้กับผู้ใช้งานทำงานแล้ว!!!\nสามารถใช้คำสั่ง ***$inactiveBotReply*** เพื่อหยุดการใช้งาน`);
+        }
+        else if (real_command === 'inactiveBotReply') {
+            isActive = false;
+            msg.channel.send('ฟังก์ชันตอบโต้กับผู้ใช้งานได้หยุดลง!');
+        }
+        else {
+            msg.reply("สามารถพิมพ์ $help เพื่อดูคำสั่งทั้งหมด");
+        }
+    }
+    GetIsActive(){
+        return isActive;
+    }
+}
--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 // Coding a Bot with discord.js
 // Discord Bots
 import { Client } from 'discord.js';
-import ReplyMSg from './all_bot_feature/text_reply.js';
-import Play from './all_bot_feature/music.js'
 import dotenv from 'dotenv';
+import ReplyMSg from './all_bot_feature/text_reply';
+import Command from './all_bot_feature/command_management';
 dotenv.config();
 
 const botnaja = new Client();
-const botnaja_reply = new ReplyMSg();
 const PREFIX_COMMAND = "$"; //to make all command begins with '$'
-const music = new Play();
-let isActive = false;
+const botnaja_reply = new ReplyMSg();
+const command_management = new Command();
 
 // login bot token is in .env file
 botnaja.login(process.env.TOKEN);
@@ -28,37 +27,9 @@ botnaja.on("message", (msg) => {
     msg.reply("อะหิ อะหิ");
   }
   if (msg.content.startsWith(PREFIX_COMMAND)) {
-    const [real_command, ...args] = msg.content
-      .trim()
-      .substring(PREFIX_COMMAND.length)
-      .split(/\s+/);
-    if (real_command === "help") {
-      msg.reply(
-        `คำสั่ง ทั้งหมดมีดังนี้
-        \n1. ***$help*** เพื่อดูคำสั่งทั้งหมด
-        \n2. ***$play <music link or music name>*** เพื่อเล่นเพลงที่ต้องการ และเพิ่มเพลงในคิว 
-        \n3. ***$stop*** เพือหยุดเพลง
-        \n4. ***$skip*** เพื่อข้ามไปเล่นเพลงถัดไป
-        \n5. ***สนใจพัฒนา BotNAJA*** สามารถกดลิ้งค https://github.com/stonksfarm-development/discord-bot`
-      );
-    }
-    else if(real_command === 'play' || real_command === 'skip' || real_command === 'stop'){
-      music.execute(msg, args, real_command, botnaja);
-    }
-    else if(real_command === 'activeBotReply'){
-      isActive = true;
-      msg.channel.send(`ฟังก์ชันตอบโต้กับผู้ใช้งานทำงานแล่ว!!!
-                      สามารถใช้คำสั่ง ***$inactiveBotReply*** เพื่อหยุดการใช้งาน`);
-    }
-    else if(real_command === 'inactiveBotReply'){
-      isActive = false;
-      msg.channel.send('ฟังก์ชันตอบโต้กับผู้ใช้งานได้หยุดลง!');
-    }
-    else {
-      msg.reply("สามารถพิมพ์ $help เพื่อดูคำสั่งทั้งหมด");
-    }
+    command_management.command(msg);
   }
   else{
-    botnaja_reply.replyMsg(msg,isActive);
+    botnaja_reply.replyMsg(msg,command_management.GetIsActive());
   }
 });
