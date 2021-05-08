ed2e401e6fbdd02fa76815f318ffa77e203e92c7
Bunyawat <stonksfarm37204@gmail.com>
2021-05-08 11:34:49 +0700

    add removerole feature

--- all_bot_feature/role.js
+++ all_bot_feature/role.js
@@ -1,14 +1,6 @@
 export default class RoleManager {
     permission = "ADMINISTRATOR";
-    excute(msg,command, args){
-        if(command === 'giverole'){
-            this.giveRole(msg,args);
-        }
-        else if(command === 'removerole'){
-            //
-        }
-    }
-    giveRole(msg, args) {
+    excute(msg, command, args) {
         const user = msg.mentions.users.first();
         if (!user) {
             msg.reply('Plz specify someone to give a role');
@@ -20,13 +12,23 @@ export default class RoleManager {
         const role = guild.roles.cache.find((role) => {
             return role.name === roleName;
         });
-        if(!role){
+        if (!role) {
             msg.reply(`there is no role with the name "${roleName}"`);
             return;
         }
         const member = guild.members.cache.get(user.id);
-        console.log(member);
-        member.roles.add(role);
-        msg.reply(`that's now user have a "${roleName} role"`);
+        if (command === 'giverole') {
+            member.roles.add(role);
+            msg.reply(`that's now user have a "${roleName} role"`);
+        }
+        else if (command === 'removerole') {
+            if(member.roles.cache.get(role.id)){
+                member.roles.remove(role);
+                msg.reply(`the user has no "${roleName} role"`);
+            }
+            else{
+                msg.reply(`the user does not have "${roleName}" role`);
+            }
+        }
     }
 }
